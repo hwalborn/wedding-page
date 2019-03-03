e59aefbb236ebc0d7896b0558fc75a037a6aceb0
hwalborn <hwalborn@gmail.com>
2019-03-03 13:57:05 -0500

    read from google sheets api and photo directory

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,8 +12,16 @@ const Header = () => {
     return (
       <div>
         <Row className="header" style={{margin: 0}}>
-          <Col md={12}>
+          <Col md={{span:6, offset:3}}>
             <Link to="/">Holt & Sara</Link>
+          </Col>
+          <Col md={3}>
+            <ButtonToolbar>
+              <DropdownButton id="drop" title="Explore" key={1} noCaret style={style}>
+                <Dropdown.Item eventKey="1" className="dropItem"><Link style={{fontSize: '1em', paddingLeft: 0}} to="/resumes/technical">Technical</Link></Dropdown.Item>
+                <Dropdown.Item eventKey="2" className="dropItem"><Link style={{fontSize: '1em', paddingLeft: 0}} to="/resumes/dance">Dance</Link></Dropdown.Item>
+              </DropdownButton>
+            </ButtonToolbar>
           </Col>
         </Row>
         <Row id="scroll-strip" />
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,24 +2,58 @@ import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
-import maine from '../images/OceanMaine.jpg'
-import sickSara from '../images/SickSara.jpg'
+import api from '../data/apiKey'
 
-const Main = () => {
-  return (
-    <div>
-    <Carousel infiniteLoop="true" autoPlay="true" >
-        <div>
-            <img src={maine}/>
-            <p className="legend">Maine</p>
-        </div>
-        <div>
-            <img src={sickSara}/>
-            <p className="legend">Sara got sea sick</p>
-        </div>
-    </Carousel>
-    </div>
-  )
+class Main extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      sheetLoaded: false,
+      aboutValues: {}
+    }
+  }
+
+  componentDidMount() {
+    if(this.state.sheetLoaded){
+      return;
+    }
+    fetch(`https://sheets.googleapis.com/v4/spreadsheets/14OsbtSgGtA911j16Y_QjX6P_etYEJ4sahge0iQn8mcw/values:batchGet?key=${api.apiKey}&ranges=Sheet1&majorDimension=ROWS`)
+    .then((resp) => {
+      resp.json()
+      .then((data) => {
+        var valueObjects = {}
+        data.valueRanges[0].values.forEach((value) => {
+          valueObjects[value[0]] = value[1]
+        })
+        this.setState(() => {
+          return {
+            sheetLoaded: true,
+            aboutValues: valueObjects
+          }
+        })
+      })
+    })
+  }
+  render(){
+  let importAll = (r) => {
+    return r.keys().map(r);
+  }
+
+  let images = importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
+  let textData = this.state.sheetLoaded ? <h1>{this.state.aboutValues.AboutSara}</h1> : null
+  let carouselImages = images.map((image, index) => {
+    return <div><img src={image} key={index}/><p className="legend">TEST</p></div>
+  })
+
+    return (
+      <div>
+        {textData}
+        <Carousel infiniteLoop={true} autoPlay={true} >
+            {carouselImages}
+        </Carousel>
+      </div>
+    )
+  }
 }
 
 export default Main
